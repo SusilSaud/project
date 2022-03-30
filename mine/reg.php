0c6dbde30f17fadde76e321e47b29a94af061b57
amritraya <97998919+amritraya@users.noreply.github.com>
2022-03-30 22:34:11 +0545

    CHANGED

--- mine/reg.php
+++ mine/reg.php
@@ -71,76 +71,79 @@
 
 
   <div class="container">
-    <div class="title">Registration</div>
-    <div class="content">
-      <form id="form1" action="" method="post">
+      <div class="title">Registration</div>
+            <div class="content">
+                <form id="form1" action="" method="post">
 
-        <div class="user-details">
-          <div class="input-box">
-            <label for="userName" class="details">Username</label>
-            <!-- name="userName" field is for database
-                id="txtUserName" field is for js validation
-                span tag inside id="userName" field is for generating error message with the help of js validation.       
-        -->
-            <input type="text" id="txtUserName" class="form-control" placeholder="Enter your username" name="userName">
-            <span class="formerror hide" id="userName">User name is required</span>
-          </div>
-          <div class="input-box">
-            <label for="email" class="details">Email</label>
-            <input type="email" id="txtEmail" class="form-control" placeholder="Enter your email" name="email">
-            <span class="formerror hide" id="email"> Email field is required</span>
-          </div>
+                  <div class="user-details">
+                    <div class="input-box">
+                      <label for="userName" class="details">Username</label>
+                      <!-- name="userName" field is for database
+                          id="txtUserName" field is for js validation
+                          span tag inside id="userName" field is for generating error message with the help of js validation.       
+                  -->
+                      <input type="text" id="txtUserName" class="form-control" placeholder="Enter your username" name="userName">
+                      <span class="formerror hide" id="userName">User name is required</span>
+                    </div>
+                    <div class="input-box">
+                      <label for="email" class="details">Email</label>
+                      <input type="email" id="txtEmail" class="form-control" placeholder="Enter your email" name="email">
+                      <span class="formerror hide" id="email"> Email field is required</span>
+                    </div>
 
-          <div class="input-box">
-            <label for="password" class="details">Password</label>
-            <input type="password" id="txtPassword" class="form-control" placeholder="Enter your password" name="password">
-            <span class="formerror hide" id="password">Password is required </span>
-          </div>
-          <div class="input-box">
-            <label for="cPassword" class="details">Confirm Password</label>
-            <input type="password" id="txtcPassword" class="form-control" placeholder="Confirm your password" name="cPassword">
-            <span class="formerror hide" id="cPassword">Confirm password is required </span>
-          </div>
-        </div>
-        <div class="input-box">
-          <label for="phone" class="details">Phone Number</label>
-          <input type="number" id="txtPhone" class="form-control" placeholder="Enter your phone number" name="phone">
-          <span class="formerror hide" id="phone">Phone number is required </span>
-        </div>
-        <div class="input-box">
-          <label for="address" class="details">Address</label>
-          <input type="text" id="txtAddress" class="form-control" placeholder="Enter your address" name="address">
-          <span class="formerror hide" id="address"> Address field is required</span>
-        </div>
-        <div class="input-box">
-          <label for="dateOfBirth" class="details">Date of birth</label>
-          <input type="date" id="txtDateOfBirth" class="form-control" placeholder="mm / dd / yyyy" name="dateOfBirth">
-          <!-- <span class="formerror hide" id="dateOfBirth"> Birth date field is required</span> -->
-        </div>
-        <div class="input-box">
-          <label for="gender" class="details">Gender</label>
-          <select id="gender" name="gender">
-            <option name="" value="gender">Select gender</option>
-            <option name="male" value="male">Male</option>
-            <option name="female" value="female">Female</option>
-            <option name="other" value="other">Other</option>
-          </select>
-          <!-- <span class="formerror hide" id="gender"> Gender selection field is required</span>  -->
-        </div>
-    </div>
-    <div class="input-box button">
-      <button type="submit" name="register_btn" class="btn btn-primary">Sign up</button>
-    </div>
-    <div class="login-signup">
-      <label for="link" class="text">Already registered?
-        <a href="login.html" class="text signup-link">Login</a>
-      </label>
-    </div>
+                    <div class="input-box">
+                      <label for="password" class="details">Password</label>
+                      <input type="password" id="txtPassword" class="form-control" placeholder="Enter your password" name="password">
+                      <span class="formerror hide" id="password">Password is required </span>
+                    </div>
+                    <div class="input-box">
+                      <label for="cPassword" class="details">Confirm Password</label>
+                      <input type="password" id="txtcPassword" class="form-control" placeholder="Confirm your password" name="cPassword">
+                      <span class="formerror hide" id="cPassword">Confirm password is required </span>
+                    </div>
+                 
+                  <div class="input-box">
+                    <label for="phone" class="details">Phone Number</label>
+                    <input type="number" id="txtPhone" class="form-control" placeholder="Enter your phone number" name="phone">
+                    <span class="formerror hide" id="phone">Phone number is required </span>
+                  </div>
+                  <div class="input-box">
+                    <label for="address" class="details">Address</label>
+                    <input type="text" id="txtAddress" class="form-control" placeholder="Enter your address" name="address">
+                    <span class="formerror hide" id="address"> Address field is required</span>
+                  </div>
+                  <div class="input-box">
+                    <label for="dateOfBirth" class="details">Date of birth</label>
+                    <input type="date" id="txtDateOfBirth" class="form-control" placeholder="mm / dd / yyyy" name="dateOfBirth">
+                    <!-- <span class="formerror hide" id="dateOfBirth"> Birth date field is required</span> -->
+                  </div>
+                  <div class="input-box" id="select">
+                    <label for="gender" class="details">Gender</label>
+                    <select id="gender" name="gender">
+                      <option name="" value="gender">Select gender</option>
+                      <option name="male" value="male">Male</option>
+                      <option name="female" value="female">Female</option>
+                      <option name="other" value="other">Other</option>
+                    </select>
+                    <!-- <span class="formerror hide" id="gender"> Gender selection field is required</span>  -->
+                  </div>
+                  </div>
+                </form>
+           
+              <div class="input-box button" >
+                <button type="submit" name="register_btn" class="btn btn-primary" id="btn">Sign up</button>
+              </div>
+              <div class="login-signup">
+                <label for="link" class="text">Already registered ?
+                  <a href="login.html" class="text signup-link">Login</a>
+                </label>
+              </div>
 
 
 
-  </div>
-  </form>
+            </div>
+          
+            
   </div>
 
 </body>
